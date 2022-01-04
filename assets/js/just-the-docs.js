e853542da2943205c1ba0c0ee4e2fbdf4ba2ab1e
b-studios <>
2022-01-04 10:28:13 +0000

    Deploying to gh-pages from @ se-tuebingen/oopsla-2022-artifact@ea9b4e7aa40150b75961cb307257f0ce6fc5f37c 🚀

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -59,7 +59,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/oopsla-2022-artifact/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
